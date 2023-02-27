4a31f3e8a9f205cd5d7e63fd412cc0c1c8b478e8
Nik <nikitama1hr@gmail.com>
2023-02-27 23:46:20 +0300

    sort/some event template

--- outlineapp/src/App.vue
+++ outlineapp/src/App.vue
@@ -5,14 +5,39 @@
   <a v-bind:href="linkUrl">bind</a>
   <a :href="linkUrl+ '/'+ someUrl">AA</a>
   <a :[atrKey]="linkUrl+ '/'+ someUrl">AA</a>
+  <div class="message" v-if="msgIsVisible"> <!--if variable true-->
+    Some message!
+  </div>
+  <div class="message" v-else-if="msgIsVisible === 2">v else message</div><!--if variable 2-->
+  <div class="message" v-else><!--if variable false-->
+    Another message!
+  </div>
+  <template v-else-if="msgIsVisible === 3">
+    <!--no render in DOM-->
+    <h3>Some msg</h3>
+  </template>
+  <template v-else-if="msgIsVisible === 4">
+    <!--no render in DOM-->
+    <h3>Some msg</h3>
+  </template>
+  <template v-else-if="msgIsVisible === 4">
+    <!--
+    * render DOM with hidde display: none
+    * not working with v-if and etc.
+    -->
+    <h2 v-show="isVisibleBlock">V Show</h2>
+  </template>
+  <div class="text-input" v-if="inputNameVisible"><input key="haha-react" type="text" placeholder="Input text"></div>
+  <div class="name-input" v-else><input  key="haha-no-react" type="text" placeholder="Input name"></div>
 </template>
-
+<!-- Event -->
+<button @click="inputNameVisible = !inputNameVisible">Change form</button>
 <script>
 
 export default {
   name: 'App',
   components: {
-  // null
+    // null
   },
   data: () => ({
     firstName: 'Nik',
@@ -23,6 +48,9 @@ export default {
     linkUrl: 'https://google.com',
     someUrl: 'https://google.com',
     atrKey: 'href',
+    msgIsVisible: 1,
+    isVisibleBlock: 1,
+    inputNameVisible: true,
   })
 };
 </script>
