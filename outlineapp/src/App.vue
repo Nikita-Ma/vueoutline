d2e442d57b2fa427001d9ef52ead228491453644
Nik <nikitama1hr@gmail.com>
2023-03-01 10:47:35 +0300

    while templv-for

--- outlineapp/src/App.vue
+++ outlineapp/src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  {{text}}
-  <!--<button @click="inputNameVisible = !inputNameVisible">Change form</button>-->
-  <button @click="onClick('some value', $event)">Change form</button>
-  <button @click.right="onClick('some value', $event)">Change form</button>
-  <!--event - it's event :DD -->
-  <a href="/" @click.prevent="onClick('some value', $event)">Change form</a>
-  <!--  no restart event reloading (e.preventDefault)-->
-    <input aria-label="test" type="text" @keyup="onKeyApp($event)">
-    <input aria-label="test" type="text" @keyup.enter="onKeyApp('Enter',$event)">
-
+  <!--  {{ text }}-->
+  <!--  <button @click="onClick('some value', $event)">Change form</button>-->
+  <!--  <button @click.right="onClick('some value', $event)">Change form</button>-->
+  <!--  &lt;!&ndash;event - it's event :DD &ndash;&gt;-->
+  <!--  <a href="/" @click.prevent="onClick('some value', $event)">Change form</a>-->
+  <!--  &lt;!&ndash;  no restart event reloading (e.preventDefault)&ndash;&gt;-->
+  <!--  <input aria-label="test" type="text" @keyup="onKeyApp($event)">-->
+  <!--  <input aria-label="test" type="text" @keyup.enter="onKeyApp('Enter',$event)">-->
+  <!--  <ul>-->
+  <!--    <li v-for="(color, index) in colors" :key="color">-->
+  <!--      {{ index }}-{{ color }}-->
+  <!--    </li>-->
+  <!--  </ul>-->
+  <ul>
+    <li v-for="color in colors" :key="color">{{ color }}</li>
+    <li v-for="(user) in users" :key="user.name">{{ user.name }}, {{ user.age }}</li>
+    <li v-for="(value, nameKey, index) in product" :key="index">
+      {{ index }}-{{ nameKey }}:{{ value }}
+    </li>
+    <input aria-label="test" type="text" @keyup.enter="addNewColor">
+    <input aria-label="test" type="text" @keyup.enter="delProp">
+  </ul>
 </template>
 <script>
 
@@ -18,18 +30,25 @@ export default {
     // null
   },
   data: () => ({
-    text: ''
-  //   firstName: 'Nik',
-  //   num: 2,
-  //   obj: {
-  //     age: 19,
-  //   },
-  //   linkUrl: 'https://google.com',
-  //   someUrl: 'https://google.com',
-  //   atrKey: 'href',
-  //   msgIsVisible: 1,
-  //   isVisibleBlock: 1,
-  //   inputNameVisible: true,
+    colors: ['orange', 'yellow', 'black'],
+    users: [{ name: 'Denis', age: 22 }, { name: 'Nik', age: 19 }],
+    product: {
+      brand: 'Apple',
+      model: 'Maxintoshi',
+      price: 'Free'
+    }
+    // text: ''
+    //   firstName: 'Nik',
+    //   num: 2,
+    //   obj: {
+    //     age: 19,
+    //   },
+    //   linkUrl: 'https://google.com',
+    //   someUrl: 'https://google.com',
+    //   atrKey: 'href',
+    //   msgIsVisible: 1,
+    //   isVisibleBlock: 1,
+    //   inputNameVisible: true,
   }),
   // save any methods, which we want use in component
   methods: {
@@ -40,6 +59,16 @@ export default {
       console.log(enterEvent, e);
       // Take this.nameComponents = anyValue
       this.text = e.target.value;
+    },
+    addNewColor(e) {
+      this.colors.push(e.target.value);
+      // where aim | index | what write
+      // for object usually
+      this.$set(this.colors, this.colors.length, e.target.value);
+    },
+    delProp(e) {
+      // delete this.product[e.target.value];
+      this.$delete(this.product, e.target.value);
     }
   }
 };
