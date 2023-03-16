4a2d74ff6c83f0e9a2ffea5b1700a9602b145458
Nik <nikitama1hr@gmail.com>
2023-03-16 22:56:13 +0300

    create component and connected

--- outlineapp/index.js
+++ outlineapp/index.js
@@ -1,7 +1,30 @@
 const app = Vue.createApp({
-    render() {
-        return Vue.h('div', {class: 'root'}, ['Hi there'])
+    data() {
+        return {
+            i: 0
+        }
+    },
+
+    template: `
+    <b-child :a="i"></b-child>
+    `,
+
+    mounted() {
+        setInterval(() => {
+            this.i++;
+        }, 500)
     }
+
+
 })
 
+app.component('b-child', {
+    props: ['a'],
+    template: `
+    This "a" prop {{a}}
+    `,
+    mounted() {
+        console.log(this.$root)
+    }
+})
 app.mount('#app')
